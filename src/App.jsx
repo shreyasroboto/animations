f1777539d70ee1e2710781ea020beb20fe48e461
Shreyas R <shreyas@roboto.studio>
2024-10-04 13:45:51 +0530

    bump

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { LoginButton } from "./components/loginButton";
 export default function App() {
   return (
-    <>
-      <div className="flex h-screen justify-center items-center">
-        <LoginButton />
-      </div>
-    </>
+    <div className="flex h-screen justify-center items-center">
+      <LoginButton />
+    </div>
   );
 }
